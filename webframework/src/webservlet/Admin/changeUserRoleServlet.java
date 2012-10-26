ed37eafd524def671e9df4b600448dffc841d7ae
anhhungxadieuuit <anhhungxadieuuit@gmail.com>
2012-10-26 12:58:01 +0000

    update

---
+++ webframework/src/webservlet/Admin/changeUserRoleServlet.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservlet.Admin;
+
+import frontend.MiddlewareHandler;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.thrift.TException;
+
+/**
+ *
+ * @author root
+ */
+public class changeUserRoleServlet extends HttpServlet {
+
+    MiddlewareHandler handler = new MiddlewareHandler();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String userID = req.getParameter("userID");
+        int userRole = Integer.parseInt(req.getParameter("userRole"));
+        String userToken = req.getParameter("userToken");
+        boolean result = false;
+        try {
+            result = handler.editUser(userID, userToken, userRole);
+        } catch (TException ex) {
+            Logger.getLogger(changeUserRoleServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        res.getWriter().println(result);
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        doGet(req, res);
+    }
+}
