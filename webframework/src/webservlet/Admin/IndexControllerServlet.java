6171d8ed0fbecf7f5fc41c4fff9690b363bf7d07
chanhlt190290 <chanhlt190290@gmail.com>
2012-10-28 10:51:05 +0000

    chanh -commit

--- webframework/src/webservlet/Admin/IndexControllerServlet.java
+++ webframework/src/webservlet/Admin/IndexControllerServlet.java
@@ -3,6 +3,9 @@ package webservlet.Admin;
 import com.google.gson.Gson;
 import com.vng.jcore.common.Config;
 import com.vng.jcore.profiler.ProfilerLog;
+import frontend.Item;
+import frontend.MiddlewareHandler;
+import frontend.Tag;
 import hapax.Template;
 import hapax.TemplateDataDictionary;
 import hapax.TemplateDictionary;
@@ -15,28 +18,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import frontend.Item;
-import frontend.MiddlewareHandler;
-import frontend.Tag;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IndexControllerServlet extends HttpServlet {
-
     private static final Logger log = LoggerFactory.getLogger(IndexControllerServlet.class);
     MiddlewareHandler handler = new MiddlewareHandler();
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
         resp.setContentType("text/html; charset=utf-8");
         boolean dbg = ("on".equals(req.getParameter("jdbg")));
         ProfilerLog profiler = new ProfilerLog(dbg);
         req.setAttribute("profiler", profiler);
         profiler.doStartLog("wholereq");
-
         try {
             String content = this.render(req);
 
@@ -46,7 +41,6 @@ public class IndexControllerServlet extends HttpServlet {
             log.error(ex.getMessage());
             this.out("Error exception: " + ex.getMessage(), resp);
         }
-
         profiler.doEndLog("wholereq");
         if (dbg) {
             String tmp = profiler.dumpLogHtml();
@@ -63,7 +57,6 @@ public class IndexControllerServlet extends HttpServlet {
         int page = 1;
         String tagID = "-1";
         int itemPerPage = Integer.valueOf(Config.getParam("paging", "itemPerPage"));
-
         try {
             if (req.getParameter("p") != null) {
                 page = Integer.parseInt(req.getParameter("p"));
@@ -71,62 +64,44 @@ public class IndexControllerServlet extends HttpServlet {
             if (req.getParameter("t") != null) {
                 tagID = req.getParameter("t");
             }
-
         } catch (Exception ex) {
             return "Error: " + ex.getMessage();
         }
-
         List<Item> listItem = this.testPaging(page, itemPerPage, tagID);
         int size = listItem.size();
-
-
-
         TemplateDataDictionary dic = TemplateDictionary.create();
-
-
         dic.setVariable("tag", tagID);
-
         dic.setVariable("title", "This is title of layout");
         if (page > 1) {
             dic.setVariable("preview", String.valueOf(page - 1));
         } else {
             dic.setVariable("preview", "1");
         }
-
-
         if (size < 10) {
             dic.setVariable("next", String.valueOf(page));
         } else {
             dic.setVariable("next", String.valueOf(page + 1));
         }
-
         dic.setVariable("itemPerPage", String.valueOf(itemPerPage));
         dic.setVariable("current", String.valueOf(page));
-
         Gson gson = new Gson();
-
         for (int i = 0; i < listItem.size(); i++) {
             TemplateDataDictionary listsection = dic.addSection("list_section");
             listsection.setVariable("itemContent", listItem.get(i).content);
             listsection.setVariable("itemID", listItem.get(i).itemID);
             listsection.setVariable("tagIDs", gson.toJson(listItem.get(i).tagsID));
         }
-
         List<Tag> listTag = getAllTag();
-
         for (int i = 0; i < listTag.size(); i++) {
             TemplateDataDictionary listTagSection = dic.addSection("listTag_section");
             listTagSection.setVariable("tagID", listTag.get(i).tagID);
             listTagSection.setVariable("tagName", listTag.get(i).tagName);
             
         }
-
         int itemTagSize = (int) handler.itemdbSize();
         int pageCount = (int) Math.ceil((float) itemTagSize / itemPerPage);
-
         int start = Math.max(page - 4, 1);
         int end = Math.min(page + 4, pageCount);
-
         for (int i = start; i <= end; i++) {
             if (i == page) {
                 TemplateDataDictionary listPageSection = dic.addSection("listPage_section");
@@ -135,15 +110,11 @@ public class IndexControllerServlet extends HttpServlet {
                 TemplateDataDictionary listPageSection = dic.addSection("listPage_section");
                 listPageSection.setVariable("page", "<a href=\"javascript:paging("+i+");\"  class=\"graybutton pagelink\" rel=\"" + i + "\">" + i + "</a>");
             }
-        }
-       
+        }    
         dic.setVariable("last", String.valueOf(pageCount));
-
         Template template = this.getCTemplate();
         String content = template.renderToString(dic);
-
         return content;
-
 
     }
 
@@ -163,10 +134,8 @@ public class IndexControllerServlet extends HttpServlet {
     }
 
     private void out(String content, HttpServletResponse resp) throws IOException {
-
         resp.addHeader("Content-Type", "text/html");
         PrintWriter out = resp.getWriter();
         out.println(content);
-
     }
 }
