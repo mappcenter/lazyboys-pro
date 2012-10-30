622b721f6326d645b9db82dd05494e6bc2a0d68d
chanhlt190290 <chanhlt190290@gmail.com>
2012-10-30 08:42:27 +0000

    chanhlt - commit

--- Middleware/src/src/getConfig.java
+++ Middleware/src/src/getConfig.java
@@ -24,10 +24,38 @@ public class getConfig {
     private static int numthread;
     private static int maxObject;
     private static int expireTime;
+    private static int numberItemIDfoTag;
+    private static int numberItemCache;
 
     /**
      * @return the numthread
      */
+    
+
+    public getConfig() throws FileNotFoundException, IOException {
+        Properties pro = new Properties();
+        pro.load(new FileInputStream("src/conf/config.ini"));
+
+        port_listen = Integer.parseInt(pro.getProperty("port_listen"));
+        port = Integer.parseInt(pro.getProperty("port"));
+        host = pro.getProperty("host");
+        ttl = Integer.parseInt(pro.getProperty("ttl"));
+        number_connection = Integer.parseInt(pro.getProperty("number_connection"));
+        numthread = Integer.parseInt(pro.getProperty("numthread"));
+        maxObject=Integer.parseInt(pro.getProperty("numberUser"));
+        expireTime=Integer.parseInt(pro.getProperty("expireTime"));
+        numberItemIDfoTag=Integer.parseInt(pro.getProperty("numberItemIDfoTag"));
+        numberItemCache=Integer.parseInt(pro.getProperty("numberItemCache"));
+        
+    }
+
+    public static synchronized getConfig getInstance() throws FileNotFoundException, IOException {
+        if (instance == null) {
+            instance = new getConfig();
+        }
+        return instance;
+    }
+    
     public int getNumthread() {
         return numthread;
     }
@@ -50,27 +78,6 @@ public class getConfig {
         ttl = aTtl;
     }
 
-    public getConfig() throws FileNotFoundException, IOException {
-        Properties pro = new Properties();
-        pro.load(new FileInputStream("src/conf/config.ini"));
-
-        port_listen = Integer.parseInt(pro.getProperty("port_listen"));
-        port = Integer.parseInt(pro.getProperty("port"));
-        host = pro.getProperty("host");
-        ttl = Integer.parseInt(pro.getProperty("ttl"));
-        number_connection = Integer.parseInt(pro.getProperty("number_connection"));
-        numthread = Integer.parseInt(pro.getProperty("numthread"));
-        maxObject=Integer.parseInt(pro.getProperty("numberUser"));
-        expireTime=Integer.parseInt(pro.getProperty("expireTime"));
-    }
-
-    public static synchronized getConfig getInstance() throws FileNotFoundException, IOException {
-        if (instance == null) {
-            instance = new getConfig();
-        }
-        return instance;
-    }
-
     public String getHost() {
         return host;
     }
@@ -87,5 +94,11 @@ public class getConfig {
     }
     public int expireTime(){
         return expireTime;
+    }
+    public int numberItemofTag(){
+        return numberItemIDfoTag;
+    }
+    public int numberItemCache(){
+        return numberItemCache;
     }
 }
