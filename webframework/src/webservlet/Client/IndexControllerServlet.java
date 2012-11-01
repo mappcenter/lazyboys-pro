ffaad58357b680115b7a6b4ffb8ac64446390f75
anhhungxadieuuit <anhhungxadieuuit@gmail.com>
2012-11-01 00:43:20 +0000

    update

--- webframework/src/webservlet/Client/IndexControllerServlet.java
+++ webframework/src/webservlet/Client/IndexControllerServlet.java
@@ -1,11 +1,8 @@
 package webservlet.Client;
 
-import IOFile.CachingIndexPage;
 import com.google.gson.Gson;
-import com.vng.jcore.common.Config;
 import com.vng.jcore.profiler.ProfilerLog;
 import frontend.Item;
-import frontend.MiddlewareFrontend;
 import frontend.MiddlewareHandler;
 import frontend.MyAppInfo;
 import frontend.MyLocalCache;
@@ -24,20 +21,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.RespectBinding;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webservlet.Admin.indexServerlet;
-import webservlet.Admin.randomItemServlet;
 import zme.api.exception.ZingMeApiException;
 import zme.api.graph.ZME_Me;
 import zme.api.oauth.ZME_AccessTokenData;
 import zme.api.oauth.ZME_Authentication;
 
 public class IndexControllerServlet extends HttpServlet {
-
     private static final Logger log = LoggerFactory.getLogger(IndexControllerServlet.class);
     public static MiddlewareHandler handler = new MiddlewareHandler();
     public ZME_Authentication zme;
@@ -53,13 +47,12 @@ public class IndexControllerServlet extends HttpServlet {
 
     @Override
     protected synchronized void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, ClientProtocolException {
-
         resp.setContentType("text/html; charset=utf-8");
         boolean dbg = ("on".equals(req.getParameter("jdbg")));
         ProfilerLog profiler = new ProfilerLog(dbg);
         req.setAttribute("profiler", profiler);
         profiler.doStartLog("wholereq");
-        long start=System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         try {
             String content = this.render(req, resp);
             this.out(content, resp);
@@ -67,12 +60,12 @@ public class IndexControllerServlet extends HttpServlet {
             log.error(ex.getMessage());
             this.out("Error exception: " + ex.getMessage(), resp);
         }
-        long end=System.currentTimeMillis();
-        System.out.println("Render page Time 1:"+(end-start)+" ms");
-        
+        long end = System.currentTimeMillis();
+        System.out.println("Render page Time 1:" + (end - start) + " ms");
+
         profiler.doEndLog("wholereq");
         String tmp = profiler.dumpLogHtml();
-        
+
         if (dbg) {
             tmp = profiler.dumpLogHtml();
             System.out.print(tmp);
@@ -104,10 +97,10 @@ public class IndexControllerServlet extends HttpServlet {
     private String render(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
         MyLocalCache mycache = new MyLocalCache();
-        long zingStart=System.currentTimeMillis();
+        long zingStart = System.currentTimeMillis();
         zme = new ZME_Authentication(MyAppInfo.getInstance().getzConfig());
         url = zme.getAuthorizedUrl(MyAppInfo.getInstance().getUrlToRedirect(), url);
-        
+
         if (req.getParameter("code") == null || req.getParameter("code").isEmpty()) {
             res.sendRedirect(url);
         } else {
@@ -134,8 +127,8 @@ public class IndexControllerServlet extends HttpServlet {
                 res.sendRedirect("/blockUser");
             }
         }
-        long zingEnd=System.currentTimeMillis();
-        System.out.println("Zing time:"+(zingEnd-zingStart)+" ms");
+        long zingEnd = System.currentTimeMillis();
+        System.out.println("Zing time:" + (zingEnd - zingStart) + " ms");
         //MiddlewareHandler.myLocalCache.CacheUserItemIDLike(me.get("id").toString());
 
 //        List<Item> listItem = this.getTopItems(req);        
@@ -160,19 +153,19 @@ public class IndexControllerServlet extends HttpServlet {
 //        String uID="5037964";
 //        String uName="thiensuhack";
 //        List<String> strIndexHtml = mycache.getCacheIndexPageWithUser(uID, uName);
-        
+
         TemplateDataDictionary dic = TemplateDictionary.create();
-        
+
         List<String> strIndexHtml = mycache.getCacheIndexPageWithUser(me.get("id").toString(), me.get("displayname").toString());
-        long start=System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         if (strIndexHtml != null && strIndexHtml.size() > 0) {
             for (int i = 0; i < strIndexHtml.size(); i++) {
                 TemplateDataDictionary listsection = dic.addSection("list_content");
                 listsection.setVariable("contentHtml", strIndexHtml.get(i));
             }
         }
-        long end=System.currentTimeMillis();
-        System.out.println("repair before render:"+(end-start)+" ms");
+        long end = System.currentTimeMillis();
+        System.out.println("repair before render:" + (end - start) + " ms");
         Template template = this.getCTemplate();
         String content = template.renderToString(dic);
         return content;
@@ -200,10 +193,8 @@ public class IndexControllerServlet extends HttpServlet {
     }
 
     private void out(String content, HttpServletResponse resp) throws IOException {
-
         resp.addHeader("Content-Type", "text/html");
         PrintWriter out = resp.getWriter();
         out.print(content);
-
     }
 }
